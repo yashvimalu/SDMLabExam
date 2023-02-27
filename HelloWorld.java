468d8f8a459b6bc425879a3b15c131e2b1e7028b
yashvimalu <maluyashvi1@gmail.com>
2023-02-27 10:20:58 +0530

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,70 @@
+package com.app;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Helo World");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * git init git add HelloWorld.java
+ * 
+ * Commit the changes:
+ * 
+ * git commit -m "Initial commit"
+ * 
+ * Create a new repository on GitHub, then add the remote repository URL to the
+ * local Git repository and push the code to GitHub: csharp
+ * 
+ * git remote add origin <github-repo-url> git push -u origin master
+ * 
+ * Modify the HelloWorld.java file to add a second greeting line, like this:
+ * 
+ * public class HelloWorld { public static void main(String[] args) {
+ * System.out.println("Hello World!"); System.out.println("Hello Git!"); } }
+ * Save and exit the file.
+ * 
+ * Commit the changes and push them to GitHub:
+ * 
+ * git add HelloWorld.java git commit -m "Add second greeting" git push origin
+ * master
+ */
